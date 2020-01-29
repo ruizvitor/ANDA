e6e5823558e1bba32357b09922e225d7feb1d2b8
Daniel Ruiz <ruiz.vitor@gmail.com>
2020-01-29 13:00:08 -0200

    minor security update due to tensflow version
    Signed-off-by: Daniel Ruiz <ruiz.vitor@gmail.com>

--- PConvInpainting/install.sh
+++ PConvInpainting/install.sh
@@ -1,3 +1,3 @@
-conda install h5py==2.8.0 keras==2.2.4 keras-applications==1.0.6 keras-preprocessing==1.0.5 matplotlib==3.0.2 numpy==1.15.4 pandas==0.23.4 scipy==1.1.0 seaborn==0.9.0 pytables==3.4.4 tensorboard==1.12.2 tensorflow==1.12.0 tqdm==4.28.1
+conda install h5py==2.8.0 keras==2.2.4 keras-applications==1.0.6 keras-preprocessing==1.0.5 matplotlib==3.0.2 numpy==1.15.4 pandas==0.23.4 scipy==1.1.0 seaborn==0.9.0 pytables==3.4.4 tensorboard==1.12.2 tensorflow==1.15.2 tqdm==4.28.1
 pip install keras-tqdm
 conda install pillow opencv
